6602f022f0964c75da8c6a2b3e54e394c19ab3ab
unknown <guillermortiz21@gmail.com>
2020-03-16 23:30:28 -0600

    file validation start

--- assets/fileParser.js
+++ assets/fileParser.js
@@ -1,7 +1,24 @@
 class FileParser{
-    constructor(file){
-        this.file = file;
+    constructor(fileText){
+        this.fileText = fileText.replace(/\r\n|\n\r|\n|\r/g, "\r\n"); // para aceptar cualquier tipo de salto de linea - retorno de carro
+        this.rows = this.fileText.split("\n");
+    }
+
+
+
+    validateFile(){
+        const regex = /^(\d+,)*(\d+)$/;
+        var i = 1;
+        this.rows.forEach(row => {
+            console.log(row);
+            if(regex.test(row)){
+                console.log("Error en la fila ", i);
+                return false;
+            }
+            i++;
+        });
+        return true;
     }
 }
 
-module.export = FileParser;
+export default FileParser;
--- assets/labyrinth.js
+++ assets/labyrinth.js
@@ -1,6 +1,12 @@
-const FileParser = require('./fileParser.js');
+import FileParser from './fileParser.js';
 const labFile = localStorage.getItem('labFile');
 const labyrinthElement = document.getElementById("labyrinth");
 
+const fileParser = new FileParser(labFile);
+if(fileParser.validateFile()){
+    console.log("Valido");
+}else{
+    console.log("No valido");
+}
 
 labyrinthElement.innerHTML = labFile
