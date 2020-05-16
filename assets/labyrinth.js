ed6ae65446d66c170d70d22d1b5aff6e5d1b607d
Guillermo Ortiz <guillermortiz21@gmail.com>
2020-05-16 13:34:13 -0500

    solver types added to menu

--- assets/labyrinth.js
+++ assets/labyrinth.js
@@ -17,7 +17,7 @@ class Labyrinth{
         this.finalState = {};
         this.characters = {};
         this.currentCharacter = {}
-        this.solverType = "Depth"; // Manual Depth UniformCost
+        this.solverType = "Depth"; // Manual Depth UniformCost Greedy AStar
         this.searchOrder = ["up", "right", "down", "left"];
         this.labyrinthWorking = false;
 
